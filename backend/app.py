7ba4cb64f92b6b3d6a2a8d3669b85e72a9f00874
JayJajoo <84397446+JayJajoo@users.noreply.github.com>
2025-05-25 00:18:42 -0400

    added req.txt in backend

--- backend/app.py
+++ backend/app.py
@@ -267,9 +267,4 @@ def download_resume():
         return jsonify({"error": f"Failed to download resume: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', 
-            port=5000, 
-            debug=True,
-            # use_reloader=False,
-            # threaded=True
-            )
+    app.run(debug=False)
